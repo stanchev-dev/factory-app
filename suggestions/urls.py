038185975a2eeb146739e13a38548b056c8554d9
Martin Stanchev <92334759+stanchev-dev@users.noreply.github.com>
2025-08-05 02:46:19 +0300

    Add manager-controlled suggestion status

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -6,5 +6,10 @@ from . import views
 urlpatterns = [
     path("", views.suggestion_list, name="suggestion_list"),
     path("vote/<int:pk>/<str:vote>/", views.vote_suggestion, name="vote_suggestion"),
+    path(
+        "status/<int:pk>/<str:status>/",
+        views.change_suggestion_status,
+        name="change_suggestion_status",
+    ),
     path("history/", views.suggestion_history, name="suggestion_history"),
 ]
