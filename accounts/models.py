b0c426b7199bb355d8222bfde0aec39465ffe268
Martin Stanchev <92334759+stanchev-dev@users.noreply.github.com>
2025-08-05 03:28:06 +0300

    Add default profile image url property

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
     class RoleChoices(models.TextChoices):
@@ -14,6 +15,11 @@ class CustomUser(AbstractUser):
     profile_picture = models.CharField(
         max_length=255,
         blank=True,
-        default="/static/icons/default_avatar.svg",
+        default=settings.DEFAULT_AVATAR_URL,
     )
+
+    @property
+    def profile_image_url(self) -> str:
+        """Return the user's profile image or the default avatar."""
+        return self.profile_picture or settings.DEFAULT_AVATAR_URL
 
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class ProfileImageURLTests(TestCase):
+    def test_returns_default_when_blank(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="alice", password="password", profile_picture=""
+        )
+        self.assertEqual(user.profile_image_url, settings.DEFAULT_AVATAR_URL)
+
+    def test_returns_custom_url_when_set(self):
+        User = get_user_model()
+        url = "/static/icons/custom.svg"
+        user = User.objects.create_user(
+            username="bob", password="password", profile_picture=url
+        )
+        self.assertEqual(user.profile_image_url, url)
